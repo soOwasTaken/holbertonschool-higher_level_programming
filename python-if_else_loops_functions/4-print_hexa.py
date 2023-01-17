13aec5962243bce6532ee4bb646c5897f63d1230
soOwasTaken <444deph12@gmail.com>
2023-01-17 15:26:10 +0100

    add of task 4

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range (0, 99):
-    print(i, '=', hex(i))
+for i in range(0, 99):
+    if i < 10:
+        print(f"{i} = 0x{i:01x}".format(i))
+        continue
+    print(f"{i} = 0x{i:02x}".format(i))
+s
