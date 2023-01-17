4e5763120f38792825834e535e8872bc0cd61c78
soOwasTaken <444deph12@gmail.com>
2023-01-17 15:09:15 +0000

    fixing 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print(f"0{i:1}".format(i), end=", ")
-        continue
-    print(f", {i:01}".format(i), end="")
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
