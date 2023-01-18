45979bcb7db9880fd968dce265f1ef70a2a28a27
soOwasTaken <444deph12@gmail.com>
2023-01-18 08:03:20 +0000

    using len?

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             str = str[:i] + chr(ord(str[i]) - 32) + str[i+1:]
-    print("{}\n".format(str), end = "")
+    print("{}\n".format(str), end="")
