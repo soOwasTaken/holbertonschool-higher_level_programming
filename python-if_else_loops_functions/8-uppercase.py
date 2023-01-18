3e099513630770519e7e3e96eed59c4adc8ce926
soOwasTaken <444deph12@gmail.com>
2023-01-18 07:57:57 +0000

    changes syntax

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for element in str:
-        if ord(element) >= 97 and ord(element)<= 122:
-            print(chr(ord(element) - 32), end = "")
+        if ord(element) >= 97 and ord(element) <= 122:
+            print(chr(ord(element) - 32), end="")
         else:
-            print(element, end ="")
-    print("\n", end ="")
+            print(element, end="")
+    print("\n", end="")
