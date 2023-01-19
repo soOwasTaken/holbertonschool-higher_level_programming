d0f31d5e01755a37d643cc88d564b45fbdcda24a
soOwasTaken <444deph12@gmail.com>
2023-01-19 04:28:06 +0000

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+argv = sys.argv[1:]
+argc = len(argv)
+
+if argc == 0:
+    print("0 arguments.")
+elif argc == 1:
+    print("1 argument:")
+else:
+    print(f"{argc} arguments:")
+
+for i, arg in enumerate(argv, start=1):
+    print(f"{i}: {arg}")
