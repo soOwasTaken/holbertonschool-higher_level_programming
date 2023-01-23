3d1ec5618a8982d7d94f69316266b08ea1f342b7
soOwasTaken <444deph12@gmail.com>
2023-01-23 08:55:23 +0000

    learning how to use row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=' ')
+            print("{:d} ".format(num), end='')
+            if num +1 == None:
+                print("{:d}".format(num), end='')
         print()
