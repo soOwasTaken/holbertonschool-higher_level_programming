e453c62b9b1eb4d5554699fd1b48ea3e658d478b
soOwasTaken <444deph12@gmail.com>
2023-01-23 09:30:19 +0000

    add of multiple_returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple1 = len(sentence)
+    if sentence == "":
+        tuple1 = 0
+        tuple2 = None
+        return tuple1, tuple2
+    else:
+        tuple1 = len(sentence)
     if sentence[0] == None:
         tuple2 = None
         return tuple1, tuple2
