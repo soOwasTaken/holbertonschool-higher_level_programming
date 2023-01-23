fac6f82f2131edd5b45648b1169796cad9871bd1
soOwasTaken <444deph12@gmail.com>
2023-01-23 09:02:01 +0000

    fixing new betty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i,row in enumerate(matrix):
-        for j,num in enumerate(row):
+    for i, row in enumerate(matrix):
+        for j, num in enumerate(row):
             print("{:d}".format(num), end=' ' if j != len(row)-1 else '')
         print()
