72fe07ea0a16b2dc711af55a8d10e2fafdd6bf46
soOwasTaken <444deph12@gmail.com>
2023-01-23 08:32:13 +0000

    learned that you need len -1 to get the last element of the list

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    i = len(my_list) - 1
+    while i >= 0:
+        num = my_list[i]
+        print("{:d}".format(num))
+        i -= 1
