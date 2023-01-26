1a04f0be3ae9e18ca410dad786ad0717397fbfd2
soOwasTaken <444deph12@gmail.com>
2023-01-26 10:17:04 +0000

    \ to add seperate line

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,8 +13,10 @@ def roman_to_int(roman_string):
     }
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_to_int_map[roman_string[i]] > roman_to_int_map[roman_string[i - 1]]:
-            result += roman_to_int_map[roman_string[i]] - 2 * roman_to_int_map[roman_string[i - 1]]
+        if i > 0 and roman_to_int_map[roman_string[i]] \
+        > roman_to_int_map[roman_string[i - 1]]:
+            result += roman_to_int_map[roman_string[i]] \
+            - 2 * roman_to_int_map[roman_string[i - 1]]
         else:
             result += roman_to_int_map[roman_string[i]]
     return result
