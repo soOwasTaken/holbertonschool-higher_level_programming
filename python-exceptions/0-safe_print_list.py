c8fce99d9c0807bb11b2458466d0ba5a562e63ff
soOwasTaken <444deph12@gmail.com>
2023-01-27 08:55:15 +0000

    adding a break in the loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed_elements = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
-            printed_elements += 1
-        print()
+            if i < sum(1 for _ in my_list):
+                print(my_list[i], end="")
+                printed_elements += 1
+            else:
+                break
     except:
         pass
+    print()
     return printed_elements
