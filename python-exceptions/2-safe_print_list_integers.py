a96a20fc2bfb900988edc5b558589500bf5ea61a
soOwasTaken <444deph12@gmail.com>
2023-01-27 09:11:16 +0000

    IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     while i < x:
         try:
-            print("{:d}".format(int(my_list[i])), end="")
+            print("{:d}".format(int(my_list[i])), end=' ')
             count += 1
         except IndexError:
             print()
+            print("An error occurred: list index out of range")
             return count
         except:
             pass
