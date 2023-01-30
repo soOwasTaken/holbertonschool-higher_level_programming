d3019a568716f5bd6c04d7bbfc3a3e6e0a114fd8
soOwasTaken <444deph12@gmail.com>
2023-01-30 17:58:42 +0000

    are we good

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,12 +3,10 @@
 
 
 class Square:
-    def __init__(self, size):
-        self.__size = 3
-
-
-"""Square that defines a square of size 3
+    """Square that defines a square of size 3
 
     Attributes:
         self (self): self
         size (:obj:`int`):size of the square."""
+    def __init__(self, size):
+        self.__size = 3
