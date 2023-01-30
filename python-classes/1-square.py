2cef15af7021096e00049998e396956824948d7c
soOwasTaken <444deph12@gmail.com>
2023-01-30 17:54:47 +0000

    are we good

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Empty class Square that defines a square"""
+
+
+class Square:
+   def __init__(self, size):
+      self.__size = 3
+"""Square that defines a square of size 3"""
