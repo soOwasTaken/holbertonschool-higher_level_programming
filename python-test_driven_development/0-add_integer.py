6d984431dec5e18b8c015121301cea0fbfaf60f4
soOwasTaken <444deph12@gmail.com>
2023-01-31 12:51:45 +0000

    add of files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """addition of 2 integer"""
+
+
 def add_integer(a, b=98):
-"""addition of 2 integer, a and b must be integer"""
+    """addition of 2 integer, a and b must be integer"""
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
