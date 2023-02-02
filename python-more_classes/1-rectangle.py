a3d12f0acb7ea3d4978d4685ba04eb2ba0344791
soOwasTaken <444deph12@gmail.com>
2023-02-02 06:36:34 +0000

    add of task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a rectangle"""
+"""Initialize the rectangle with optional width and height"""
 
 
-class Square:
-    """Empty class Square that defines a rectangle"""
-    pass
+class Rectangle:
+    """Initialize the rectangle with optional width and height"""
+
+    def __init__(self, widght=0, height=0):
+        self.width = widght
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
