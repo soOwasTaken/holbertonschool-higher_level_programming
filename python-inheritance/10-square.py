60b8325c27a356c72aa33599bc1e1e193dfc6ee8
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:09:15 +0000

    test

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,35 +1,10 @@
 #!/usr/bin/python3
-"""
-Module that defines a class BaseGeometry
-"""
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-class Rectangle(BaseGeometry):
-    """Class Rectangle that inherits from BaseGeometry"""
-    def __init__(self, width, height):
-        """Instantiation with width and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return a string representation of the rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+"""Module that contains a class BaseGeometry"""
+Rectangle = __import__('9-rectangle').BaseGeometry
 
 class Square(Rectangle):
     """Class Square that inherits from Rectangle"""
+
     def __init__(self, size):
         """Instantiation with size"""
         self.integer_validator("size", size)
