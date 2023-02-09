81b051072eaea9101615680c3e18d8df3c9aeb34
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:42:52 +0000

    fixed format

--- python-input_output/6-save_from_json_file.py
+++ python-input_output/6-save_from_json_file.py
@@ -2,6 +2,7 @@
 """Function that creates an Object from a “JSON file”"""
 import json
 
+
 def load_from_json_file(filename):
     """Function that creates an Object from a “JSON file”"""
     with open(filename, 'r') as file:
