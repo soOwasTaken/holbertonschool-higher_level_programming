6fe9a002ea1cb65a44faee0b800f4969e491af8a
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:05:02 +0000

    using import instead of recoding BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-"""
-Module that defines a class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+"""Module that contains a class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Class Rectangle that inherits from BaseGeometry"""
