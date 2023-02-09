799f599bf3b13e5fb0ef24619a867581032a667d
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:40:25 +0000

    fixing format

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""function that returns the JSON representation of an objectimport json"""
+"""function that returns the JSON representation of an object"""
 import json
 
 
 def to_json_string(my_obj):
-    """function that returns the JSON representation of an objectimport json"""
+    """function that returns the JSON file of an object"""
     return json.dumps(my_obj)
