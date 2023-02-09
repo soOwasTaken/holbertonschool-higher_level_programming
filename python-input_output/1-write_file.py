525a8d0c9f1e75bd55f111e3a32ecb30468d329a
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:38:09 +0000

    add of folder for project 3

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the num of char"""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
