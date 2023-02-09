c7420235e69094a52f0454ac69c4e06e172b7fb2
soOwasTaken <444deph12@gmail.com>
2023-02-09 11:40:50 +0000

    fixing format on task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" returns an object (Python data structure) represented by a JSON"""
+""" returns an object represented by a JSON"""
 import json
 
 def from_json_string(my_str):
-    """ returns an object (Python data structure) represented by a JSON"""
+    """ returns an object represented by a JSON"""
     return json.loads(my_str)
