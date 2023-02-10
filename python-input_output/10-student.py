4517990a34bc6c071e22a9b725011b052547ade1
soOwasTaken <444deph12@gmail.com>
2023-02-10 13:32:22 +0000

    add of task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """student class with 3 arguments"""
 
+
 class Student:
     """student class with 3 arguments"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
