bcd27484d447f3aada76aa0fd2448a6b9acbb565
soOwasTaken <444deph12@gmail.com>
2023-02-10 13:26:35 +0000

    add of task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""returns a dictionary representation"""
-Student = __import__('9-student').Student
+"""student class with 3 arguments"""
 
+class Student:
+    """student class with 3 arguments"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self, attrs=None):
-    if attrs is None:
-        return self.__dict__
-    else:
-        return {k: v for k, v in self.__dict__.items() if k in attrs}
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
