f0189a7dfbab33d77e458daaac0eaaac0bc862ed
soOwasTaken <444deph12@gmail.com>
2023-02-10 13:24:27 +0000

    add of task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns a dictionary representation"""
+Student = __import__('9-student').Student
+
+
+def to_json(self, attrs=None):
+       if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
