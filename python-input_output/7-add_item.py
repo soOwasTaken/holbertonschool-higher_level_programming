a5192fbdd0e741d3e62060ae5107888bfd7c22c9
soOwasTaken <444deph12@gmail.com>
2023-02-10 11:59:46 +0000

    add of task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
 """a script that adds all arguments to a Python list, and then save them to a file"""
 import sys
-import json
 
 
-def save_to_json_file(my_obj, filename):
-    """save to json"""
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """load from json"""
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-filename = "add_item.json"
-args = sys.argv[1:]
-try:
-    items = load_from_json_file(filename)
-except:
-    items = []
-items += args
-save_to_json_file(items, filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save-to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+    filename = "add_item.json"
+    args = sys.argv[1:]
+    try:
+        items = load_from_json_file(filename)
+    except:
+        items = []
+    items += args
+    save_to_json_file(items, filename)
