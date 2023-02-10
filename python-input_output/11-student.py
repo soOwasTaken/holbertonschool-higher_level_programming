fcceb5416312d031b30a9a39f963d4a8fde7756a
soOwasTaken <444deph12@gmail.com>
2023-02-10 13:41:18 +0000

    add of task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""student class with 3 arguments"""
+
+
+class Student:
+    """student class with 3 arguments"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for key in json:
+            setattr(self, key, json[key])
+
