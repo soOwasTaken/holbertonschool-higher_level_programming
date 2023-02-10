2b6e385f815d6f057c9fec90762382819db8f05f
soOwasTaken <444deph12@gmail.com>
2023-02-10 13:25:37 +0000

    add of task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,7 +4,7 @@ Student = __import__('9-student').Student
 
 
 def to_json(self, attrs=None):
-       if attrs is None:
-            return self.__dict__
-        else:
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+    if attrs is None:
+        return self.__dict__
+    else:
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
