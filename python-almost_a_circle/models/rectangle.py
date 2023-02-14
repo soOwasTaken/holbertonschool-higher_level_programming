ac6076318e0fddaf94502ed7f1cf80e3dce84e3d
soOwasTaken <444deph12@gmail.com>
2023-02-14 12:22:28 +0000

    add of task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Class Square that inherits from Rectangle"""
+Base = __import__('Base').Base
+
+
+class Rectangle(Base):
+    "class Rectangle that inherit Base"
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self, value):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
