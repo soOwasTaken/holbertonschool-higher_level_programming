35e48e22ef69e71e77432263c0f6304293c96361
soOwasTaken <444deph12@gmail.com>
2023-02-15 09:41:56 +0000

    add of update for the rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -29,3 +29,25 @@ class Square(Rectangle):
     def area(self):
         """area of square"""
         return self.size * self.size
+
+    def update(self, *args, **kwargs):
+        if args:
+            num_args = len(args)
+            if num_args > 0:
+                self.id = args[0]
+            if num_args > 1:
+                self.size = args[1]
+            if num_args > 2:
+                self.x = args[2]
+            if num_args > 3:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
