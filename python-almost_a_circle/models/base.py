6edb44b9e090db1447594d21e3e39e4e858af4b7
soOwasTaken <444deph12@gmail.com>
2023-02-15 10:28:17 +0000

    add of json handle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,7 +4,6 @@ import json
 
 
 class Base:
-    """class Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -34,3 +33,39 @@ class Base:
 
         with open(filename, "w") as file:
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation"""
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances from a JSON file"""
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+        except FileNotFoundError:
+            return []
+
+        list_dicts = cls.from_json_string(json_string)
+        instances = [cls.create(**dict) for dict in list_dicts]
+        return instances
