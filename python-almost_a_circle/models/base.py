219729308ed209b5fdcf1c0f9df96cc21f40262e
soOwasTaken <444deph12@gmail.com>
2023-02-15 10:09:29 +0000

    add of json handle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """class Base"""
 import json
-from rectangle import Rectangle
-from square import Square
 
 
 class Base:
@@ -18,17 +16,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """json representation of all objects"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+        """Returns the JSON string representation
+           of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return []
         else:
-            dict_list = []
-            for dict_obj in list_dictionaries:
-                if isinstance(dict_obj, dict):
-                    if "size" in dict_obj:
-                        obj = Square(1)
-                    else:
-                        obj = Rectangle(1, 1)
-                    obj.update(**dict_obj)
-                    dict_list.append(obj.to_dictionary())
-            return json.dumps(dict_list)
+            return json.dumps(list_dictionaries)
