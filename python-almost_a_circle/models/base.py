7b83dd7166a3e9533eb6f05136654cc9c64b1453
soOwasTaken <444deph12@gmail.com>
2023-02-15 10:14:31 +0000

    add of json handle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,10 +16,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation
-           of list_dictionaries
-        """
+        """Returns the JSON string representation"""
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
