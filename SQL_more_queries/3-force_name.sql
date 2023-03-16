07f101c4fd254ebec2c52a4628e8af519c03af3a
soOwasTaken <444deph12@gmail.com>
2023-03-16 09:32:09 +0000

    add of task 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -6,4 +6,3 @@ CREATE TABLE IF NOT EXISTS force_name (
 );
 UPDATE force_name SET name='Python is cool' WHERE id=1;
 INSERT INTO force_name (id, name) VALUES (2, 'Holberton School');
-SELECT * FROM force_name;
