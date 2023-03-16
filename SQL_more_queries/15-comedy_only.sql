10356293a491b9be8d4a504fbc1bf70b9a78cb61
soOwasTaken <444deph12@gmail.com>
2023-03-16 10:49:08 +0000

    add of task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
