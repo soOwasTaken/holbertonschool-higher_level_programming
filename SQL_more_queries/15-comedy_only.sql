77c27774992b82c10b11e736eba430acc5ab7ad9
soOwasTaken <444deph12@gmail.com>
2023-03-16 10:14:57 +0000

    add of task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@ FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
 INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
