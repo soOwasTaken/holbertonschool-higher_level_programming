4dcc2729a18e1ba692f8d6aedf6efb4c6ce29ef9
soOwasTaken <444deph12@gmail.com>
2023-03-16 09:36:21 +0000

    add of task 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -2,6 +2,4 @@
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
 );
-INSERT INTO force_name (id, name) VALUES (1, 'Force A') ON DUPLICATE KEY UPDATE name = 'Force A';
