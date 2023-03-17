ed2e69715f7d11f94843b26beac02352ac5ec741
soO <444deph12@gmail.com>
2023-03-17 06:42:44 -0700

    add of task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
-"""script that list all states from database x"""
-import sys
-import MySQLdb
+"""Lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == '__name__':
-    db = MySQLdb(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states;")
-    result = cursor.fetchall()
-    for row in result:
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from states ORDER BY id ASC")
+
+    results = cursor.fetchall()
+    for row in results:
         print(row)
+
+    cursor.close()
+    connection.close()
