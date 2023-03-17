1437be6aa3c06912074793c577a3d94e76a3636e
soO <444deph12@gmail.com>
2023-03-17 10:54:12 -0700

    add of task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-lists all State objects from the database hbtn_0e_6_usa
+lists all State objects from the database test_7
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     # get the arguments
