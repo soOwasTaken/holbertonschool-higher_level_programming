50bcf1095bc6d59223644eaa64230cd8a99a54b8
soO <444deph12@gmail.com>
2023-04-11 03:32:29 -0700

    add of task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
     }
   }
 }
