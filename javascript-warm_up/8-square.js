3561e1ecfc13d3b43879cb6d41916efadd2bfe0f
soO <444deph12@gmail.com>
2023-04-11 02:29:27 -0700

    learnings loops

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  if (size <= 0) {
+    process.exit();
+  }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
