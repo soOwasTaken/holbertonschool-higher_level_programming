3db232319897d28ca2605248f1a6841e24d3a381
soO <444deph12@gmail.com>
2023-04-11 05:06:01 -0700

    add of task 8, fixed semistandard

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-exports.esrever = function(list) {
+exports.esrever = function (list) {
   const reversedList = [];
   for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
     reversedList[j] = list[i];
   }
   return reversedList;
-}
+};
