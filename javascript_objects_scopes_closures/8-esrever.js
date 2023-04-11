1143f978fec896e98b2eef9a38a6f3c87a10f9ea
soO <444deph12@gmail.com>
2023-04-11 05:04:54 -0700

    add of task 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-exports.esrever = function (list) {
-  return (list.reverse());
-};
+exports.esrever = function(list) {
+  const reversedList = [];
+  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
+    reversedList[j] = list[i];
+  }
+  return reversedList;
+}
