19c5feec9c09cce19df8b7f6d8b38db9b81a4e31
soO <444deph12@gmail.com>
2023-04-11 02:04:55 -0700

    fixing situation with 0 input

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 2) {
   console.log('Argument not found');
+} if (process.argv.length === 0) {
+  console.log('No argument');
 } else {
   console.log('Argument found');
 }
