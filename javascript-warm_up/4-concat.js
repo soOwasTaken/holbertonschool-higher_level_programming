38dbe45d28be29bfd50ae12b14982765994d30b2
soO <444deph12@gmail.com>
2023-04-11 02:13:41 -0700

    0 argument situation removed

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
-if (arg1 === undefined) {
-  console.log('No argument');
-} else {
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${arg1} is ${arg2}`);
