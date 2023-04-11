19f3654bc798793d547318480328340efc6bfed7
soO <444deph12@gmail.com>
2023-04-11 02:06:39 -0700

    fixing if else

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 2) {
-  console.log('Argument not found');
-} if (process.argv.length === 0) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
