74345d54393f80f22f6b87f5f5c73b13d553fe1d
soO <444deph12@gmail.com>
2023-04-11 02:40:59 -0700

    fixing semistandard validation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   } else if (n <= 1) {
     return 1;
   } else {
-    return n * factorial (n - 1);
+    return n * factorial(n - 1);
   }
 }
 
