170d40fa5292471797e2f068c0d3ca537ddd3604
soO <444deph12@gmail.com>
2023-04-17 10:44:36 -0700

    add of task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
