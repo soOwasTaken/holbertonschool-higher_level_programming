67596f593e4fc3331416c542349dd48e970d5d9d
soO <444deph12@gmail.com>
2023-04-16 22:05:10 -0700

    add of task 0, definitly fixed standard this time ?

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf8", (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
   } else {
-    const lines = data.split("\n");
+    const lines = data.split('\n');
     for (let i = 0; i < lines.length; i++) {
       console.log(lines[i]);
     }
