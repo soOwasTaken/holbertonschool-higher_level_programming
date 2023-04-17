d5d8b8715a9ae98dd165b89b6906344d9d872c95
soO <444deph12@gmail.com>
2023-04-16 22:02:20 -0700

    add of task 0, setup standard format on save

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require("fs");
 const filePath = process.argv[2];
 
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  } else {
+    const lines = data.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      console.log(lines[i]);
     }
-    else {
-        const lines = data.split('\n');
-        for (let i = 0; i < lines.length; i++) {
-            console.log(lines[i]);
-        }
-    }
+  }
 });
