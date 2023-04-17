5125ff8f01bdf704ed072351f2d67bd0e9cae50d
soO <444deph12@gmail.com>
2023-04-16 21:50:56 -0700

    forgot the part with the argv

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const filePath = process.argv[2];
 
-fs.readFile('cisfun', 'utf8', (err, data) => {
+
+fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
