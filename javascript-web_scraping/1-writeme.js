5e71ba2d3181a886a033b7b6a4dfe49a899ccfd6
soO <444deph12@gmail.com>
2023-04-17 09:42:07 -0700

    add of task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -7,7 +7,5 @@ const content = process.argv[3];
 fs.writeFile(filePath, content, (err) => {
   if (err) {
     console.error(`Error writing file: ${err}`);
-  } else {
-    console.log(content);
   }
 });
