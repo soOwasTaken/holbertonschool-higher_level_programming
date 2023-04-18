e2c0f533357975ffceb6af0c13e34c6e86103687
soO <444deph12@gmail.com>
2023-04-18 02:12:45 -0700

    add of task 4

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("#toggle_header").click(function () {
+    const header = $("header");
+    if (header.hasClass("red")) {
+      header.removeClass("red");
+      header.addClass("green");
+    } else {
+      header.removeClass("green");
+      header.addClass("red");
+    }
+  });
+});
