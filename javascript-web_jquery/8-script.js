34fd17d1e874731b9c833dfb8466fbd34606a424
soO <444deph12@gmail.com>
2023-04-18 02:18:27 -0700

    add of task 8

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $.getJSON(
+    "https://swapi-api.hbtn.io/api/films/?format=json",
+    function (data) {
+      $.each(data.results, function (index, movie) {
+        $("#list_movies").append("<li>" + movie.title + "</li>");
+      });
+    }
+  );
+});
