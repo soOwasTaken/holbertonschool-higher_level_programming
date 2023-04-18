b312c3c452b3272119a45d860c86d52ef6ed39f1
soO <444deph12@gmail.com>
2023-04-18 01:49:48 -0700

    add of task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require("request");
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(`Error writing file: ${err}`);
+  }
+
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  tasks.forEach((task) => {
+    if (task.completed) {
+      const userId = task.userId;
+      if (completedTasksByUser[userId]) {
+        completedTasksByUser[userId]++;
+      } else {
+        completedTasksByUser[userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
